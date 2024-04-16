9a0f36ced9b29d329ba7450bc2491333fcb34f9c
Herman Havva <108483440+hermanhavva@users.noreply.github.com>
2024-04-16 21:40:06 +0300

    Three new classes initialised
    In Graph.cs there is three classes, two are nested inside the Graph class. They are all will be needed to implement graph class

--- Reachability_matrix_bfs_dfs/Reachability_matrix_bfs_dfs/Graph.cs
+++ Reachability_matrix_bfs_dfs/Reachability_matrix_bfs_dfs/Graph.cs
@@ -2,5 +2,28 @@ namespace Reachability_matrix_bfs_dfs;
 
 public class Graph
 {
-    
+
+    public List<Node> Vertexes = new List<Node>();
+    public class Node // nested class Node
+    {
+        public List<Node> AdjacencyList = new List<Node>();
+
+        public void AddNeighbor(Node neighbor)
+        {
+            AdjacencyList.Add(neighbor);
+        }
+    }
+
+    public class Edge // nested class Edge
+    {
+        public Node AdjacentVertex1;
+        public Node AdjacentVertex2;
+
+        public Edge(Node adjacentVertex1, Node adjacentVertex2)
+        {
+            AdjacentVertex1 = adjacentVertex1;
+            AdjacentVertex2 = adjacentVertex2;
+        }
+    }
 }
+
--- Reachability_matrix_bfs_dfs/Reachability_matrix_bfs_dfs/Program.cs
+++ Reachability_matrix_bfs_dfs/Reachability_matrix_bfs_dfs/Program.cs
@@ -1,3 +1,3 @@
-﻿
+﻿using Reachability_matrix_bfs_dfs;
 
-Console.WriteLine("Hello, World!");
+var vertex1 = new Graph.Node();
