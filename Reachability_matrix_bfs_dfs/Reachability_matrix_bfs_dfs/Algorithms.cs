e0a9d22c76c78f494fd1317d6090cbc2c9e8cbce
Herman Havva <108483440+hermanhavva@users.noreply.github.com>
2024-04-20 15:18:38 +0300

    remove unnecessary class and add time gathering code

--- Reachability_matrix_bfs_dfs/Reachability_matrix_bfs_dfs/Algorithms.cs
+++
@@ -1,6 +0,0 @@
-namespace Reachability_matrix_bfs_dfs;
-
-public class Algorithms
-{
-    
-}
--- Reachability_matrix_bfs_dfs/Reachability_matrix_bfs_dfs/Program.cs
+++ Reachability_matrix_bfs_dfs/Reachability_matrix_bfs_dfs/Program.cs
@@ -1,24 +1,37 @@
 ﻿using Reachability_matrix_bfs_dfs;
 
-//  testing the functionality of graph generator
+//  analysing and gathering execution time per algorithm
 var graphGenerator = new GraphGenerator();
+TimeSpan totalTimeBfsOnMatrix = new();
+TimeSpan totalTimeBfsOnLists = new();
+TimeSpan totalTimeDfsOnMatrix = new();
+TimeSpan totalTimeDfsOnLists = new();
 
-var graph = graphGenerator.GenerateGraph(20, 4);
 
-var dfs = new BreadthFirstSearch(graph);
-var reachMatrix = dfs.GetReachabilityMatrixAdjacencyLists(graph);
+BreadthFirstSearch bfs = new();
+DepthFirstSearch dfs = new();
+int numberOfExperiments = 40;  // 40 повторів експериментів
 
-for (int row = 0; row < 4; row++)
+for (int counter = 1; counter <= numberOfExperiments; counter++)
 {
-    for (int column = 0; column < 4; column++)
+    Graph graph = graphGenerator.GenerateGraph(50, 100);   //  сюди вводиш параметри графа
+    
+    totalTimeBfsOnMatrix += bfs.GetReachabilityMatrixAdjacencyMatrix(graph).executanceTime;
+    totalTimeBfsOnLists += bfs.GetReachabilityMatrixAdjacencyLists(graph).executanceTime;
+
+    totalTimeDfsOnMatrix += dfs.GetReachabilityMatrixAdjacencyMatrix(graph).executanceTime;
+    totalTimeDfsOnLists += dfs.GetReachabilityMatrixAdjacencyLists(graph).executanceTime;
+    if (counter == numberOfExperiments)
     {
-        Console.Write(reachMatrix[row, column] ? 1 : 0);
-
+        Console.WriteLine($"Average time for BFS on Matrix: {totalTimeBfsOnMatrix/numberOfExperiments}\n");
+        Console.WriteLine($"Average time for BFS on Lists: {totalTimeBfsOnLists/numberOfExperiments}\n");
+        
+        Console.WriteLine($"Average time for DFS on Matrix: {totalTimeDfsOnMatrix/numberOfExperiments}\n");
+        Console.WriteLine($"Average time for DFS on Lists: {totalTimeDfsOnLists/numberOfExperiments}\n");
     }
-    Console.WriteLine();
-}
+} 
 
-graph.PrintAdjacencyLists();
+
 
 
 
